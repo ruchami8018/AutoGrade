d922182a342c401123d4848a3b88f30143295ba2
ruchami8018 <ruchami508018@gmail.com>
2025-07-10 20:46:38 +0300

    update logo again

--- AutoGradeClient/src/pages/Landing.tsx
+++ AutoGradeClient/src/pages/Landing.tsx
@@ -130,8 +130,9 @@ export default function Landing() {
           <div className="relative hidden lg:block">
             <div className="absolute -top-8 -right-8 w-64 h-64 bg-gradient-to-br from-blue-400/20 to-purple-400/20 rounded-full blur-3xl"></div>
             <div className="absolute -bottom-12 -left-12 w-80 h-80 bg-gradient-to-br from-indigo-400/20 to-cyan-400/20 rounded-full blur-3xl"></div>
+            {/*5*/}
             <img
-              src="https://my-auto-grade-testpnoren.s3.us-east-1.amazonaws.com/LOGO_AUTO_GRADE_4.png"
+              src="https://my-auto-grade-testpnoren.s3.us-east-1.amazonaws.com/LOGO_AUTO_GRADE!.png"
               alt="דשבורד AutoGrade"
               className="relative rounded-xl shadow-2xl border-8 border-white"
             />
